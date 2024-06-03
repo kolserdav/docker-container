bb797d9b57ffe7a011c4fa7995e7d56087022fd1
kolserdav <serega12101983@gmail.com>
2024-06-03 11:26:04 +0700

    fixed actions

--- src/login.sh
+++ src/login.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-set -a 
-source .env
-set +a
+if [ -f .env ]; then
+    set -a 
+    source .env
+    set +a
+fi
 
 echo $GITHUB_TOKEN | docker login ghcr.io -u USERNAME --password-stdin

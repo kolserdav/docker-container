80364d96790a82b51d9e3490e4f63cbce69428f2
kolserdav <serega12101983@gmail.com>
2024-06-03 14:04:16 +0700

    add test

--- Makefile
+++ Makefile
@@ -1,20 +1,16 @@
 compose:
 	docker compose build --no-cache
 build:
-	./src/scripts/build.sh
+	./src/build.sh
 start:
 	docker compose up -d
 logs:
 	docker logs scratch
 restart:
 	make build && make start
-rootfs:
-	./src/scripts/create-rootfs.sh $(RELEASE)
-remount:
-	./src/scripts/remount.sh
 multiarch-install:
 	./src/multiarch-install.sh
 login:
 	./src/login.sh
 test:
-	docker build ./tests
+	./src/tests/run.sh
---
+++ src/tests/run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+set -e
+
+oss=$(sh $(dirname "$0")/../os.sh)
+oss_arr=($(echo $oss | tr "," "\n"))
+i=0
+ERRORS=()
+
+for os in "${oss_arr[@]}"
+do
+    echo "Test OS $os ..."
+    releases=$(sh $(dirname "$0")/../$os/releases.sh)
+    releases_arr=($(echo $releases | tr "," "\n"))
+
+    for release in "${releases_arr[@]}"
+    do
+      image=test-$os:$release
+      docker build --build-arg="RELEASE=$release" -f $(dirname "$0")/Dockerfile.$os -t $image .
+      i=$((i+1))
+      port=800$i
+      container_name=test-$os-$release
+      docker run -p "$port:80" --name $container_name -d test-$os:$release
+      res=$(curl "http://127.0.0.1:$port")
+      echo "Request result:"
+      echo "$res"
+      if [[ $res == *"<center>nginx</center>"* ]]; then
+        echo "Success test $image"
+      else
+        echo "Failed test $image"
+        ERRORS+=($image)
+      fi
+
+      docker stop $container_name
+      docker rm $container_name
+      docker image rm -f $image
+    done
+done
+
+if [ ${#a[@]} != 0 ]; then
+  echo "Test finished with ${#a[@]} errors"
+  exit 1
+else
+  echo "Test successfully finished with 0 errors"
+fi
